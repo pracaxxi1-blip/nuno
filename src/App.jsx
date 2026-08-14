07439d71c2830074fca88602d2321b091b966898
pracaxxi1-blip <pracaxxi1@gmail.com>
2026-08-14 03:49:06 -0300

    Refactor routing to use Router and add Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import CreateRequest from './pages/CreateRequest';
 import ClientDashboard from './pages/ClientDashboard';
 import LojistaDashboard from './pages/LojistaDashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import CreateRequest from './pages/CreateRequest';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
+        {/* Rota Inicial e Login */}
         <Route path="/" element={<Login />} />
-        <Route path="/create-request" element={<CreateRequest />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Rotas do Cliente */}
+        <Route path="/client-dashboard" element={<ClientDashboard />} />
         <Route path="/my-requests" element={<ClientDashboard />} />
+        <Route path="/create-request" element={<CreateRequest />} />
+
+        {/* Rotas do Lojista */}
         <Route path="/lojista-dashboard" element={<LojistaDashboard />} />
+
+        {/* Rotas do Administrador */}
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+
+        {/* Rota Fallback (Redireciona para o login se a URL não existir) */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
